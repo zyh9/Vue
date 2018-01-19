1a3fc6541be23063852af0b79c3c6045dd5ec541
zhangyanhui <1124596031@qq.com>
2018-01-19 17:27:41 +0800

    hui

--- Question/store/types.js
+++ Question/store/types.js
@@ -1,9 +1,3 @@
-// export const START_ACTION = 'START_ACTION';
-// export const PROBLEM_LIST = 'PROBLEM_LIST';
-// export const PROBLEM_CLEAN = 'PROBLEM_CLEAN';
-// export const QUESTION_LIST = 'QUESTION_LIST';
-// export const QUESTION_CLEAN = 'QUESTION_CLEAN';
-
 import keymirror from 'keymirror';
 
 let types = keymirror({
