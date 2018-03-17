18640a40465f7254f132930a593dbea4a22af0cf
zhangyanhui <1124596031@qq.com>
2018-03-17 09:27:45 +0800

    hui

--- Login/wxAuth.js
+++ Login/wxAuth.js
@@ -36,28 +36,32 @@ export let wxAuth = {
     //演示 && 正式
     isShow(){
         let obj = this.filter()
-        //isShow 地址栏无参数； show 走演示； formal 走正式
-        return obj=='nodata'?'isShowErr':obj.activityId=='0'?{url:obj,isshow:'show'}:{url:obj,isshow:'formal'}
+        /**
+         * @return 'isShowErr' //地址栏无参数
+         * @return {isShow:'show'} //走演示
+         * @return {isShow:'formal'} //走正式
+        */
+        return obj=='nodata'?'isShowErr':obj.activityId=='0'?{url:obj,isShow:'show'}:{url:obj,isShow:'formal'}
     },
-    //platform 所在场景；  auth 是否授权； wxData 微信授权接口函数
+    /**
+     * @param platform 所在场景
+     * @param auth 是否授权
+     * @param wxData 微信授权接口函数
+    */
     scenes(platform,auth,wxData){
         if(platform==1){//微信端活动
             if(!auth){
                 wxData()
             }else{//授权成功之后
                 let obj = this.filter();
-                // if (navigator.userAgent.indexOf('MicroMessenger') > -1) {//微信端
-                //     if(obj=='nodata'){
-                //         return 'paramErr';//地址栏无参数
-                //     }else{
-                //         if(!(['openid'] in obj)&&auth){
-                //             //问卷答题针对公众，地址栏参数无token={$token}
-                //             if(['token'] in obj && obj.token!='{$token}'){
-                //                 return true;//可直接参与活动，调用用户信息
-                //             }else return 'noToken';//token不存在，显示登录框
-                //         }else return 'openId';//openid存在，显示登录框
-                //     } 
-                // }else return 'browser';//浏览器打开微信端活动，给出提示
+                /**
+                 * 问卷答题针对公众，地址栏参数无token={$token}
+                 * @return 'browser' //浏览器打开微信端活动，给出提示
+                 * @return 'paramErr' //地址栏无参数
+                 * @return 'openId' //openid存在，显示登录框
+                 * @return 'noToken' //token不存在，显示登录框
+                 * @return true //可直接参与活动，调用用户信息
+                */
                 return navigator.userAgent.indexOf('MicroMessenger') > -1?obj=='nodata'?'paramErr':!(['openid'] in obj)&&auth?['token'] in obj && obj.token!='{$token}'?true:'noToken':'openId':'browser';
             }
         }else return 'app';//app端活动   
