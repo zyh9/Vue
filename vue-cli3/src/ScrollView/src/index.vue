b171be52987bbc98483d84957195a4f9537b76c9
zyh <1124596031@qq.com>
2020-04-22 10:36:29 +0800

    增加better-scroll方法

--- vue-cli3/src/ScrollView/src/index.vue
+++ vue-cli3/src/ScrollView/src/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="scroll-view-con">
-        <div class="scroll-view wrapper" ref="wrapper">
-            <div class="scroll_list content">
-                <div class="scroll_item" v-for="(v,i) in 10" :key="i" @click="handleSelect(i)">
+        <div class="scroll-view wrap" ref="wrap">
+            <div class="scroll_list" ref="list">
+                <div class="scroll_item" v-for="(v,i) in 10" :key="i" @click="handleSelect(i)" ref="item">
                     <span :class="{'active':selectIndex==i}">选项{{i}}</span>
                 </div>
             </div>
@@ -22,14 +22,38 @@
             }
         },
         mounted() {
-            this.scroll = new BScroll(this.$refs.wrapper, {
-                scrollX: true,
-                click: true,
+            this.$nextTick(() => {
+                this.InitTabScroll();
             })
         },
         methods: {
+            InitTabScroll() {
+                let width = 0
+                for (let i = 0; i < 10; i++) {
+                    width += this.$refs.item[i].getBoundingClientRect().width;
+                }
+                this.$refs.list.style.width = `${width}px`
+                this.$nextTick(() => {
+                    if (!this.scroll) {
+                        this.scroll = new BScroll(this.$refs.wrap, {
+                            startX: 0,
+                            click: true,
+                            scrollX: true,
+                            scrollY: false,
+                            eventPassthrough: 'vertical'
+                        });
+                    } else {
+                        this.scroll.refresh()
+                    }
+                })
+            },
             handleSelect(i) {
+                // better-scroll 提供三个动画 swipe swipeBounce bounce
+                // let scrollX = Math.floor(this.$refs.item[i].getBoundingClientRect().left);
+                // console.log(scrollX)
+                // this.scroll.scrollTo(-scrollX, 0, 200,'swipe')
                 if (this.selectIndex == i) return;
+                this.scroll.scrollToElement(this.$refs.item[i], 300, true, true, 'swipe')
                 this.selectIndex = i;
             }
         },
